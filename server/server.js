a9ec7926c9f92a547f23cc8836cb36075f459b1e
Krystyn Smith <krskrsmith@gmail.com>
2022-01-03 21:47:21 -0600

    updated port

--- server/server.js
+++ server/server.js
@@ -33,5 +33,8 @@ app.get('*', (req, res) => {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
